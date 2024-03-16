f254fd3958d3824fdf767b821bc3cd11992cdace
control_delta <controld3785@gmail.com>
2024-03-16 14:17:32 +0900

    Merge Upload Method into Index Page (to prepare for the actual server environment)

--- main.go
+++ main.go
@@ -8,15 +8,36 @@ import (
 	"time"
 )
 
-func index(writer http.ResponseWriter, _ *http.Request) {
-	content, err := os.ReadFile("./web/form.html")
-	if err != nil {
-		println(err)
-	}
+func index(writer http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		content, err := os.ReadFile("./web/form.html")
+		if err != nil {
+			println(err)
+		}
 
-	_, err2 := fmt.Fprintf(writer, string(content))
-	if err2 != nil {
-		return
+		_, err2 := fmt.Fprintf(writer, string(content))
+		if err2 != nil {
+			return
+		}
+	case "POST":
+		b, _ := io.ReadAll(req.Body)
+
+		now := time.Now()
+		millis := now.UnixMilli()
+
+		filename := fmt.Sprintf("./adofai%d.zip", millis)
+		err := os.WriteFile(filename, b, 0644)
+		go process(filename, millis)
+
+		if err != nil {
+			println(err)
+		}
+
+		_, err2 := fmt.Fprintf(writer, "Success")
+		if err2 != nil {
+			return
+		}
 	}
 }
 
@@ -32,30 +53,9 @@ func mainScript(writer http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func upload(writer http.ResponseWriter, req *http.Request) {
-	b, _ := io.ReadAll(req.Body)
-
-	now := time.Now()
-	millis := now.UnixMilli()
-
-	filename := fmt.Sprintf("./adofai%d.zip", millis)
-	err := os.WriteFile(filename, b, 0644)
-	go process(filename, millis)
-
-	if err != nil {
-		println(err)
-	}
-
-	_, err2 := fmt.Fprintf(writer, "Success")
-	if err2 != nil {
-		return
-	}
-}
-
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/main.js", mainScript)
-	http.HandleFunc("/upload", upload)
 
 	err := http.ListenAndServe("localhost:3676", nil)
 	println(err)
--- web/main.js
+++ web/main.js
@@ -6,12 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
         const reader = new FileReader()
 
         reader.onload = () => {
-            fetch("/upload", {
+            fetch("/", {
                 method: "POST",
                 body: reader.result
             })
                 .then((response) => response.text())
                 .then((result) => console.log(result))
+            input.value = ""
         }
 
         reader.readAsArrayBuffer(input.files[0])
