84b30666698f0ec1c263db143e4d00b49e7b41cb
control_delta <controld3785@gmail.com>
2024-03-16 13:25:45 +0900

    Change Unzip Function to 7zip (to make unzip more stable)

--- process.go
+++ process.go
@@ -181,6 +181,7 @@ func process(filename string, id int64) {
 
 	err := unzipSource(filename, dest)
 	if err != nil {
+		log.Fatal(err)
 		return
 	}
 
@@ -198,10 +199,10 @@ func process(filename string, id int64) {
 	adofaiLevelStr := string(trimmedBytes)
 
 	adofaiLevelStr = convertToValidJSON(adofaiLevelStr)
-	err = os.WriteFile("log.txt", []byte(adofaiLevelStr), 0644)
+	/*err = os.WriteFile("log.txt", []byte(adofaiLevelStr), 0644)
 	if err != nil {
 		return
-	}
+	}*/
 
 	var adofaiLevelJson map[string]interface{}
 	err = json.Unmarshal([]byte(adofaiLevelStr), &adofaiLevelJson)
@@ -292,75 +293,10 @@ func process(filename string, id int64) {
 }
 
 func unzipSource(filename string, destination string) error {
-	reader, err := zip.OpenReader(filename)
-
-	if err != nil {
-		return err
-	}
-	defer func(reader *zip.ReadCloser) {
-		err := reader.Close()
-		if err != nil {
-			return
-		}
-	}(reader)
-
-	destination, err = filepath.Abs(destination)
-	if err != nil {
+	cmd := exec.Command("7z", "x", filename, "-o./"+destination)
+	if err := cmd.Run(); err != nil {
 		return err
 	}
 
-	for _, f := range reader.File {
-		err := unzipFile(f, destination)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func unzipFile(file *zip.File, destination string) error {
-	filePath := filepath.Join(destination, file.Name)
-	if !strings.HasPrefix(filePath, filepath.Clean(destination)+string(os.PathSeparator)) {
-		return fmt.Errorf("invalid file path: %s", filePath)
-	}
-
-	if file.FileInfo().IsDir() {
-		if err := os.MkdirAll(filePath, os.ModePerm); err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
-		return err
-	}
-
-	destinationFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
-	if err != nil {
-		return err
-	}
-	defer func(destinationFile *os.File) {
-		err := destinationFile.Close()
-		if err != nil {
-			return
-		}
-	}(destinationFile)
-
-	zippedFile, err := file.Open()
-	if err != nil {
-		return err
-	}
-	defer func(zippedFile io.ReadCloser) {
-		err := zippedFile.Close()
-		if err != nil {
-			return
-		}
-	}(zippedFile)
-
-	if _, err := io.Copy(destinationFile, zippedFile); err != nil {
-		return err
-	}
 	return nil
 }
